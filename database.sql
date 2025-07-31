fc97987b9d3ffc3314e48ca29022f69c24c3b002
zack6yuan <zack6yuan@gmail.com>
2025-07-31 09:36:58 -0500

    update.

--- database.sql
+++ database.sql
@@ -1,8 +1,15 @@
 -- CREATES A DATABASE TO FILTER EPISODES BY COLOR, THEME, AND LENGTH
 CREATE DATABASE IF NOT EXISTS ETL_JOY
 
-CREATE TABLE colors (
-
+CREATE TABLE IF NOT EXISTS colors (
+    painting_index INT PRIMARY KEY,
+    img_src VARCHAR(100),
+    painting_title VARCHAR(50),
+    season INT,
+    episode INT,
+    num_colors INT,
+    youtube_src VARCHAR(100),
+    colors
 )
 
 CREATE TABLE subject_matter (
