5e2189789245ee01fd55e62eb970bf6dfc0e79ed
zack6yuan <zack6yuan@gmail.com>
2025-08-04 14:58:51 -0500

    update doc and planning.

--- server/getBroadcasts.js
+++ server/getBroadcasts.js
@@ -11,3 +11,7 @@ fs.createReadStream('../data/broadcasts.csv')
     .on('end', () => {
         console.log(results);
     });
+
+// Need to update the data so that every one
+// does not say a walk in the woods
+// need to update the key per episode
