969bbe51b725c4314e8b2eede35080dc3c493cb6
zack6yuan <zack6yuan@gmail.com>
2025-08-05 16:01:23 -0500

    updates.

--- sql/load_broadcasts.sql
+++ sql/load_broadcasts.sql
@@ -1,2 +1,6 @@
-INSERT INTO broadcasts (painting_title, Date)
-VALUES ("A Walk in the Woods", "January 11, 1983");
+INSERT INTO broadcasts (title, air_date)
+VALUES (
+    "A Walk in the Woods", STR_TO_DATE('January 11, 1983', '%M %d, %Y')
+    "Mount McKinley", STR_TO_DATE('January 11, 1983',  '%M %d, %Y')
+    "Ebony Sunset", STR_TO_DATE('January 18, 1983',  '%M %d, %Y')
+);
